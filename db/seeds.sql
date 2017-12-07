f520a731cff2516eabf46e36a7ea8759958eab28
Abhijit <abhijitkghosh29@gmail.com>
2017-12-06 22:23:31 -0500

    changed Seed Sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,4 @@
 INSERT INTO burgers (Burger_Name,Devoured) VALUES ('Veggie Burger',FALSE);
-INSERT INTO burgers (Burger_Name,Devoured) VALUES ('Veggie iceberg Burger',TRUE);
+INSERT INTO burgers (Burger_Name,Devoured) VALUES ('BK Chicken Burger',FALSE);
+INSERT INTO burgers (Burger_Name,Devoured) VALUES ('Chicken Maharaja Mac',FALSE);
+INSERT INTO burgers (Burger_Name,Devoured) VALUES ('Chicken Tikki Burger',FALSE);
